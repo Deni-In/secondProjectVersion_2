cbb2868d0e82664c997b3a4b812b6f18b5a3edb9
deni-in <deni.akhmatukaev@gmail.com>
2021-06-17 16:20:06 +0300

    fixed post controller

--- controllers/post-controller/get.posts.js
+++ controllers/post-controller/get.posts.js
@@ -1,23 +1,20 @@
 const Post = require("../../models/Post");
 
 const getAllPosts = async (req, res) => {
-  const Post = require("/models/Post");
   const list = await Post.find();
 
   res.json(list);
 };
 
 const getPostById = async (req, res) => {
-  const Post = require("/models/Post");
   const item = await Post.findById(req.params.id);
 
   res.json(item);
 };
 
 const getPostsFromCategory = async (req, res) => {
-  const Post = require("/models/Post");
   const postsFromCategory = await Post.find({
-    category: req.params.id,
+    category: req.params.categoryId,
   });
   res.json(postsFromCategory);
 };
