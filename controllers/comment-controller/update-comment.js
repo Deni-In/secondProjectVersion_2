9aadf6fe718e45534087057f0cdfab2472b907ca
Tashu <t.r.soltamuradov@mail.ru>
2021-06-17 17:26:22 +0300

    fix-comments

--- controllers/comment-controller/update-comment.js
+++ controllers/comment-controller/update-comment.js
@@ -1,19 +1,16 @@
 const Comment = require("../../models/Comment");
 
 const patchComment = async (req, res) => {
-  try {
-    await Comment.updateOne(
-      { _id: req.body.id },
-      {
-        $set: {
-          ...req.body,
-        },
-      }
-    )
-    res.json("Комментарий изменен");
-  } catch (e) {
-    console.log(e);
-  }
+  await Comment.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        ...req.body,
+      },
+    }
+  )
+  res.json("Комментарий изменен");
+
 };
 
 module.exports = patchComment;
