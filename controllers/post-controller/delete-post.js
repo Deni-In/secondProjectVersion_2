ce0139ef82f7d48fd0f569b51dd33cb8184f164d
deni-in <deni.akhmatukaev@gmail.com>
2021-06-18 11:23:21 +0300

    прописаны комменты для поста

--- controllers/post-controller/delete-post.js
+++ controllers/post-controller/delete-post.js
@@ -1,9 +1,9 @@
-const Post = require("../../models/Post");
+const Post = require("../../models/Post");   // подключаем модель поста
 
-const deletePost = async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  post.delete();
-  res.json("Post deleted");
+const deletePost = async (req, res) => {  // создаем функцию удаления поста
+  const post = await Post.findById(req.params.id);  // ищем пост по ID
+  post.delete(); // удаляем пост
+  res.json("Post deleted");   // выводим сообщение об удалении
 };
 
-module.exports = deletePost;
+module.exports = deletePost;  // экспортируем функцию
